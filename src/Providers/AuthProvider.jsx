87842d1a7642fb1d171cd16c307d2555e800ef24
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-06-15 18:43:24 +0600

    Feat: set firebase auth provider

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,90 @@
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { app } from "../Firebase/Firebase.config";
+
+export const CreateContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    // loader
+    const [loader, setLoader] = useState(true);
+
+    // Register
+    const register = (email, password) => {
+        setLoader(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    // Set User Name, PhotoURL
+    const namePhotoUrl = (name, photo) => {
+        setLoader(true);
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo
+        })
+    }
+
+    // login with password
+    const logIn = (email, password) => {
+        setLoader(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // Google Log in method
+    const logInWithMedia = (arg) => {
+        if (arg === 'google') {
+            const googleProvider = new GoogleAuthProvider();
+            setLoader(true);
+            return signInWithPopup(auth, googleProvider);
+        }
+        else if (arg == 'gitHub') {
+            const gitHubProvider = new GithubAuthProvider();
+            setLoader(true);
+            return signInWithPopup(auth, gitHubProvider);
+        }
+    }
+
+    // Sign Out
+    const logOut = () => {
+        setLoader(true);
+        return signOut(auth);
+    }
+
+    // Get User information
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser);
+
+            const userEmail = currentUser?.uid || user?.uid;
+            const loaderUser = { uId: userEmail };
+            console.log('UserEmail', currentUser);
+            setLoader(false); // Set loader to false after authentication state is determined
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, []);
+
+
+    // console.log(user);
+
+    const authInfo = {
+        user,
+        setUser,
+        loader,
+        register,
+        namePhotoUrl,
+        logIn,
+        logInWithMedia,
+        logOut,
+    }
+    return (
+        <CreateContext.Provider value={authInfo}>
+            {children}
+        </CreateContext.Provider>
+    );
+};
+
+export default AuthProvider;
