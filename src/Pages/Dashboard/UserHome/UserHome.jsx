0ce454e065e575d37a6843e1040572fb7e2954be
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-06-20 19:43:51 +0600

    Upd: userHome

--- src/Pages/Dashboard/UserHome/UserHome.jsx
+++ src/Pages/Dashboard/UserHome/UserHome.jsx
@@ -1,9 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const UserHome = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
 
-    
+    const axiosSecure = useAxiosSecure();
+    const { data: userInfo = [] } = useQuery({
+        queryKey: ['userInfo', user.email],
+        queryFn: async () =>{
+            const res = axiosSecure.get(`/users/${user.email}`)
+        }
+    })
+
+
 
     return (
         <div>
